f524b84cecbf9cc94129ad88af99411229bcaaeb
Sergio Acosta <sergio.acosta@wunderman.com>
2018-11-07 13:29:16 -0300

    [WUNBRAVIVOM-3567] - Alteração do legal

--- sources/frontend/_development/gulpfile.js/tasks/default.js
+++ sources/frontend/_development/gulpfile.js/tasks/default.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var runSequence = require('run-sequence');
 
 var defaultTask = function defaultTask(cb) {
-	runSequence('clean', ['fonts', 'images', 'icons', 'data', 'htaccess'], ['scripts', 'styles', 'html'], 'watch', cb);
+	runSequence('clean', ['fonts', 'images', 'icons', 'data'], ['scripts', 'styles', 'html'], 'watch', cb);
 };
 
 gulp.task('default', defaultTask);
