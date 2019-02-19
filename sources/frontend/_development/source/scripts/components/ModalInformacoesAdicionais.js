3e84be65d9f73e23c7d1919a40b8ea5ca20602c3
Lopes <jonatas.lopes@wunderman.com>
2019-02-19 15:40:37 -0300

    [FIX] correção de detalhes de texto

--- sources/frontend/_development/source/scripts/components/ModalInformacoesAdicionais.js
+++ sources/frontend/_development/source/scripts/components/ModalInformacoesAdicionais.js
@@ -72,7 +72,7 @@ class ModalnformacoesAdicionais {
 
             $(".tab__content[data-main-content=3]").html(`<ul>
             <li>
-                <a class="regulamento" href="https://www.vivo.com.br/portalweb/ShowPropertyServlet?nodeId=/UCMRepository/CONTRIB_138766&_ga=2.260582477.1980575863.1538515923-298680962.1534272275&_gac=1.157856200.1537808383.Cj0KCQjwlqLdBRCKARIsAPxTGaVFbGTNLt_3EMjFNxUE9aqYZYjfwUwGYoq-DJFVFiNQgtWNvexXe7IaAibAEALw_wcB">
+                <a class="regulamento" href="https://www.vivo.com.br/portalweb/ShowPropertyServlet?nodeId=/UCMRepository/CONTRIB_138766&_ga=2.260582477.1980575863.1538515923-298680962.1534272275&_gac=1.157856200.1537808383.Cj0KCQjwlqLdBRCKARIsAPxTGaVFbGTNLt_3EMjFNxUE9aqYZYjfwUwGYoq-DJFVFiNQgtWNvexXe7IaAibAEALw_wcB" target="_blank">
                     PROMOÇÃO VIVO CONTROLE DIGITAL VII
                 </a>
             </li>
