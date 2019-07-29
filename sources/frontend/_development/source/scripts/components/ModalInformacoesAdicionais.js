a7fbdccf71b3b6d239ee795531876c941d1585f6
Vanessa Ariel <vanessa.ariel24@gmail.com>
2019-07-29 15:21:26 -0300

    Remoção de frase para plano anual

--- sources/frontend/_development/source/scripts/components/ModalInformacoesAdicionais.js
+++ sources/frontend/_development/source/scripts/components/ModalInformacoesAdicionais.js
@@ -88,6 +88,14 @@ class ModalnformacoesAdicionais {
                     </ul>
                 `);
 
+                $(".tab__content[data-main-content=2]").html(`
+                <ul>
+                    <li>A franquia de internet é válida pelo período de um mês;</li>
+                    <li>Após atingir o limite de dados do plano contratado, a internet será interrompida até a renovação do seu benefício, que é renovado mensalmente;</li>
+                    <li>As ligações são ilimitadas para chamadas locais e longa distância usando o <strong>código 15</strong> para qualquer Vivo ou fixo;</li>
+                    <li>As ligações locais para celulares de outras operadoras são ilimitadas;</li>
+                </ul>
+            `);
                 $(".tab__content[data-main-content=3]").html(`
                     <ul>
                         <li>
@@ -116,6 +124,16 @@ class ModalnformacoesAdicionais {
                     </ul>
                 `);
 
+                $(".tab__content[data-main-content=2]").html(`
+                <ul>
+                    <li>A franquia de internet é válida pelo período de um mês;</li>
+                    <li>Após atingir o limite de dados do plano contratado, a internet será interrompida até a renovação do seu benefício, que é renovado mensalmente;</li>
+                    <li>As ligações são ilimitadas para chamadas locais e longa distância usando o <strong>código 15</strong> para qualquer Vivo ou fixo;</li>
+                    <li>As ligações locais para celulares de outras operadoras são ilimitadas;</li>
+                    <li>As chamadas de longa distância com o <strong>código 15</strong> para celulares de outras operadoras têm o valor de R$0,50/minuto;</li>
+                </ul>
+            `);
+
                 $(".tab__content[data-main-content=3]").html(`
                     <ul>
                         <li>
@@ -132,18 +150,6 @@ class ModalnformacoesAdicionais {
                 `);
             }
         }
-
-
-
-        $(".tab__content[data-main-content=2]").html(`
-            <ul>
-                <li>A franquia de internet é válida pelo período de um mês;</li>
-                <li>Após atingir o limite de dados do plano contratado, a internet será interrompida até a renovação do seu benefício, que é renovado mensalmente;</li>
-                <li>As ligações são ilimitadas para chamadas locais e longa distância usando o <strong>código 15</strong> para qualquer Vivo ou fixo;</li>
-                <li>As ligações locais para celulares de outras operadoras são ilimitadas;</li>
-                <li>As chamadas de longa distância com o <strong>código 15</strong> para celulares de outras operadoras têm o valor de R$0,50/minuto;</li>
-            </ul>
-        `);
 
 
         $(".tab__content[data-main-content=4]").html(`
